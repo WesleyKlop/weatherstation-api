e04bfd098842bfd556fe5ed9edffd3b04e9c2ebc
Wesley Klop <wesley19097@gmail.com>
2021-03-16 11:20:03 +0100

    Fix init script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,13 +2,14 @@
 
 set -uo pipefail
 
-COMMAND="$1"
+COMMAND="${1:?"Specify a command to run"}"
+FLAG="${2:-''}"
 
 if [ "$COMMAND" = "migrate" ]; then
   exec diesel migration run
 fi
 
-if [ "$COMMAND" = "server" ] && [ -z "$2" ] && [ "$2" = "-m" ]; then
+if [ "$COMMAND" = "server" ] && [ "$FLAG" = "-m" ]; then
   diesel migration run
 fi
 
