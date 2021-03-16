e69cd22feccc9d234087cac85820e3baaab5afbb
Wesley Klop <wesley19097@gmail.com>
2021-03-16 11:30:28 +0100

    Use correct id function

--- migrations/2021-03-11-194145_create_measurements/up.sql
+++ migrations/2021-03-11-194145_create_measurements/up.sql
@@ -2,7 +2,7 @@ create extension if not exists "uuid-ossp";
 
 create table measurements
 (
-    id             UUID      not null default gen_random_uuid() primary key,
+    id             UUID      not null default uuid_generate_v4() primary key,
 
     humidity       float     not null,
     temperature    float     not null,
