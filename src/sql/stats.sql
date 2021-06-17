456fc640710e92b5d8fd27ebbc28107dd57b2e39
Wesley Klop <w.klop@inthere.nl>
2021-06-17 21:35:01 +0200

    Don't round averages

--- src/sql/stats.sql
+++ src/sql/stats.sql
@@ -1,10 +1,10 @@
-select date_trunc('hour', created_at)                        as moment,
-       round(avg(humidity)::numeric, 2)::double precision    as humidity,
-       min(humidity)                                         as min_humidity,
-       max(humidity)                                         as max_humidity,
-       round(avg(temperature)::numeric, 2)::double precision as temperature,
-       min(temperature)                                      as min_temperature,
-       max(temperature)                                      as max_temperature
+select date_trunc('hour', created_at) as moment,
+       avg(humidity)                  as humidity,
+       min(humidity)                  as min_humidity,
+       max(humidity)                  as max_humidity,
+       avg(temperature)               as temperature,
+       min(temperature)               as min_temperature,
+       max(temperature)               as max_temperature
 from measurements
 group by moment
 order by moment desc;
